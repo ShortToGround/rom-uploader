9f659480cbce0cc3a9af247df967cdf18fe2c60f
James <github@jamessimmons.me>
2023-05-18 15:47:56 -0500

    Abandoned the idea of external SRAM, now it writes to the mem IC after every chunk is received

--- avr/README.md
+++ avr/README.md
@@ -1,5 +1,5 @@
 This is a work in progress.
 
-When converting from the original programmer code to the streaming programmer code I have somehow broken the functionality.
+When converting from the original programmer code to the streaming programmer code I have somehow broken the core functionality.
 
-It has something to do with the z80 and the SRAM isolation.
+It has something to do with the z80 and the SRAM isolation. The data is streamed to the RAM just fine but when letting the z80 take control again something goes wrong.
---
+++ avr/programmer/include/programmer.h
@@ -0,0 +1,20 @@
+#define SHIFT_DATA 2
+#define SHIFT_CLK 3
+#define SHIFT_LATCH 4
+#define EEPROM_D0 5
+#define EEPROM_D7 12
+#define WRITE_EN 13
+
+
+#define MAX_ROM_SIZE 32767 // current max due to shift register hardware limit
+// TODO: Have the AVR chip send the buffer size it uses and make this dynamic on the PC end
+#define SER_BUFF_SIZE 64
+#define MAX_DATA_SIZE SER_BUFF_SIZE-3
+#define FALSE 0
+#define TRUE 1
+#define RAM 0
+#define ROM 1
+#define RAM_WR_DELAY 1
+#define ROM_WR_DELAY 10
+#define INTERNAL_RAM 0
+#define EXTERNAL_RAM 1
