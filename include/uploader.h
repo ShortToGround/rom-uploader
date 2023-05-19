d43c9b024320554aeaf786d414ab8a8307bc83ac
James <github@jamessimmons.me>
2023-05-19 11:46:57 -0500

    changed value of MAX_ROM_SIZE

--- include/uploader.h
+++ include/uploader.h
@@ -20,6 +20,6 @@
 // TODO: Have the programming device send the buffer size it uses and make this dynamic on the PC end
 #define BUFFER_SIZE 64 + 1 // +1 for null terminator on output
 #define DATA_MAX BUFFER_SIZE-3 // -3 accounts for the header and footer added to the data chunk when sending
-#define MAX_ROM_SIZE 32000
+#define MAX_ROM_SIZE 32767
 #define DATA_PACKET_FLAG_ON 1
 #define DATA_PACKET_FLAG_OFF 0
