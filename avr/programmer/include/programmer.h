839d57b9504d538b96b59f7838d88dc97d61aa4f
James <github@jamessimmons.me>
2025-01-07 16:28:53 -0600

    Added in ASCII control chars and redefined some custom project defines

--- avr/programmer/include/programmer.h
+++ avr/programmer/include/programmer.h
@@ -1,18 +1,27 @@
+// Shift Register Controls
 #define SHIFT_DATA 2
 #define SHIFT_CLK 3
 #define SHIFT_LATCH 4
 #define EEPROM_D0 5
 #define EEPROM_D7 12
-#define WRITE_EN 13
-#define CPU_BUS_REQ A4
-#define CPU_RESET A5
 
-#define Z80_INTERRUPT A2 // This does NOT work on an Arduino Nano, A6 can only be an analog input
+#define WRITE_EN        A0
+#define EEPROM_OE       A1
+#define EEPROM_CS       A2
+#define CPU_BUS_REQ     A3
+#define CPU_BUS_ACK     A4
+#define CPU_IO_REQ      A5
+#define SHIFT_REG_OE    A6
+#define CPU_RESET       A7
 
-#define MAX_ROM_SIZE 32767 // current max due to shift register hardware limit
+
+// Programmer Config
 // TODO: Have the AVR chip send the buffer size it uses and make this dynamic on the PC end
+#define MAX_ROM_SIZE 16383 // current max due to shift register hardware limit
 #define SER_BUFF_SIZE 64
-#define MAX_DATA_SIZE SER_BUFF_SIZE-3
+#define MAX_DATA_SIZE SER_BUFF_SIZE - 4
+
+// Defines
 #define FALSE 0
 #define TRUE 1
 #define RAM 0
@@ -21,3 +30,11 @@
 #define ROM_WR_DELAY 10
 #define INTERNAL_RAM 0
 #define EXTERNAL_RAM 1
+
+// Flow Control Chars
+#define SOH 0x01
+#define EOT 0x04
+#define ACK 0x06
+#define NAK 0x15
+#define ETB 0x17
+#define CAN 0x18
